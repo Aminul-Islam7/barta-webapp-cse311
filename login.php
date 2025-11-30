f055ccee6e8c59a82518cfbbb90c6bdb6cc9b6fa
Aminul-Islam7 <aminulfardin7@gmail.com>
2025-11-30 19:54:12 +0600

    feat: Add css variables, theme tokens

--- login.php
+++ login.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Barta Login</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
+
 <?php
 // Login for Tween/Parent
 ?>
